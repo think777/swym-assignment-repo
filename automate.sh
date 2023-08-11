ec3e370f1df891218e83f2e10cc36da3eef08518
blacjac21 <blacjac21@gmail.com>
2023-08-11 14:28:10 +0530

    automate final

--- automate.sh
+++ automate.sh
@@ -6,7 +6,7 @@ if ! command -v docker &> /dev/null; then
     # Install Docker (add any installation steps based on your OS)
     # For example, on Ubuntu:
     sudo apt-get update
-    sudo apt-get install -y docker.io
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 fi
 
@@ -15,8 +15,7 @@ if ! command -v docker-compose &> /dev/null; then
     echo "docker-compose is not installed. Installing docker-compose..."
     # Install docker-compose (add any installation steps based on your OS)
     # For example, on Ubuntu:
-    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+    sudo apt-get install docker-compose-plugin
 fi
 
 # Navigate to the directory containing your docker-compose.yml file
