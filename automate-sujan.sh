4bc3e69aa2db9e681572e88dba224f2131cf57cf
blacjac21 <blacjac21@gmail.com>
2023-08-11 14:26:02 +0530

    automate

--- automate-sujan.sh
+++ automate-sujan.sh
@@ -18,9 +18,16 @@ if ! command -v docker-compose &> /dev/null; then
 fi
 
 # Navigate to the directory containing your docker-compose.yml file
-cd /home/college/Documents/swym/doc2
+cd /home/college/Documents/swym
 
 # Start the Python container using docker-compose
 sudo docker-compose up -d
 
 echo "Python container started using docker-compose."
+
+
+# Sleep for the desired duration (e.g., 2 days)
+sleep 2d
+
+# Stop and remove the container
+docker-compose down
